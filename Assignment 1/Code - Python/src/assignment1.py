22bbd354c1e55dcd8a54650f785e6cd513327b84
Ben McCamish <anothersubject@gmail.com>
2015-10-05 11:22:03 -0700

    Fixed another bug

--- Assignment 1/Code - Python/src/assignment1.py
+++ Assignment 1/Code - Python/src/assignment1.py
@@ -32,20 +32,20 @@ def l2_loss(N, y, w, x, l):
 	print (y[0] - w.transpose()*x[0])
 	print np.dot(w.transpose(),x[0])
 	for i in xrange(0,N):
-		gradientError += np.pow((y[i] - np.dot(w,x[i]),2) + l*LA.norm(w)
-	
-	#return gradientError
+		gradientError += math.pow((y[i] - np.dot(w.transpose(),x[i])),2) + l*LA.norm(w)
+
+	return gradientError
 
 def l2gradientDescentStep(y, w, x, l):
     return (y - (x * w.T).T) * x + l * w
 
 def gradientDescent(N, y, w, x, l, a, epsilon):
-    gradient = 1
-    while(abs(gradient) > epsilon):
-        gradient = l2_loss(N, y, w, x, l)
-        w = w - a*gradient
+	gradient = 1
+	while(abs(gradient) > epsilon):
+		gradient = l2_loss(N, y, w, x, l)
+		w = w - a*gradient
 
-    return w
+	return w
 
 def problem1():
 	pass
@@ -57,23 +57,23 @@ def problem3():
 	pass
 
 def main():
-    #starting variables can be changed later to be command line if we want
+	#starting variables can be changed later to be command line if we want
 
-    x, y = generateVariables(trainingData)
-    w = generateInitialW(x)
-    N = x.shape[0]
-    l = 1
-    a = .5
-    epsilon = .0001
-    #print w
-    #print x
-    #print y
+	x, y = generateVariables(trainingData)
+	w = generateInitialW(x)
+	N = x.shape[0]
+	l = 1
+	a = .5
+	epsilon = .0001
+	#print w
+	#print x
+	#print y
 
-    #test = l2gradientDescentStep(y, w, x, l)
-    testProblem = gradientDescent(N, y, w, x, l, a, epsilon)
-    #problem1()
-    #problem2()
-    #problem3()
+	#test = l2gradientDescentStep(y, w, x, l)
+	testProblem = gradientDescent(N, y, w, x, l, a, epsilon)
+	#problem1()
+	#problem2()
+	#problem3()
 
 # Giving Python the main it deserves
 if __name__ == "__main__": 
