d52d1eb857948a76fe0a777022023061499dd20c
Ben McCamish <anothersubject@gmail.com>
2015-10-21 10:15:21 -0700

    Cleaning things up.

--- Assignment 2/Code - Python/Implementation2_Code.py
+++ Assignment 2/Code - Python/Implementation2_Code.py
@@ -7,26 +7,34 @@ from sys import argv
 from copy import copy, deepcopy
 from numpy.linalg import inv
 import random
-script, newsgrouplabels, vocabulary, data_labels, data_data = argv #Read in files from command line
 
+# Not sure what script is for...
+#script = 'data/'
+newsgrouplabels = 'data/newsgrouplabels.txt'
+vocabulary = 'data/vocabulary.txt'
+testLabels = 'data/test.label'
+testData = 'data/test.data'
+trainLabels = 'data/train.label'
+trainData = 'data/train.data'
 
 #########################################################################################################################
 ############ 									Take in our data files                           ########################
 ############ Returns: Group_Labels array, Vocab array, Data_Labels 1-D array, Data_Data 2-D array########################
-def Function1(newsgrouplabels, vocabulary, data_labels, data_data):
-	Group_Labels = []
-	Vocab = []
+def readData(newsgrouplabels, vocabulary, data_labels, data_data):
+	newsGroup = list()
+	vocab = list()
+
 	for line in open(newsgrouplabels,'r').readlines():
-		Group_Labels.append(line.strip())
+		newsGroup.append(line.strip())
 	
 	for line in open(vocabulary,'r').readlines():
-		Vocab.append(line.strip())
+		vocab.append(line.strip())
 	
-	Data_Labels = np.genfromtxt(data_labels,delimiter='')
+	dataLabels = np.genfromtxt(data_labels,delimiter='')
 	
-	Data_Data = np.genfromtxt(data_data,delimiter='')
+	actualData = np.genfromtxt(data_data,delimiter='')
 
-	return Group_Labels, Vocab, Data_Labels, Data_Data
+	return newsGroup, vocab, dataLabels, actualData
 	
 ###############################################################################################################################################################
 #######Create a 2-D array that shows # of word occurrences per class, also 2-D array that counts number of documents containing a certain word#################
@@ -72,8 +80,27 @@ def Class_Word_Matrix(Group_Labels, Vocab, Data_Labels, Data_Data):
 def Bernouli_Laplace(Document_Word_Occur):	
 	print 'nothing'
 	
+def problem1():
+	pass
+
+def problem2():
+	pass
+
+def problem3():
+	pass
 	
-	
+def main():
+	newsGroup, vocab, dataLabels, actualData = readData(newsgrouplabels, vocabulary, data_labels, data_data)
+	Class_WC, Document_Word_Occur, Doc_Num_Each_Class = Class_Word_Matrix(Group_Labels, Vocab, Data_Labels, Data_Data)
+
+	#problem1()
+	#problem2()
+	#problem3()
+
+
+# Giving Python the main it deserves
+if __name__ == "__main__": 
+	main()	
 	
 	
 Group_Labels, Vocab, Data_Labels, Data_Data = Function1(newsgrouplabels, vocabulary, data_labels, data_data)
