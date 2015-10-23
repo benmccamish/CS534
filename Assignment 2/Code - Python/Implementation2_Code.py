d4ed085a37b8fe763cf9b7d9b59da5e89f9a57ce
lassettc <lassettc@onid.oregonstate.edu>
2015-10-23 11:30:08 -0700

    Update Implementation2_Code.py

--- Assignment 2/Code - Python/Implementation2_Code.py
+++ Assignment 2/Code - Python/Implementation2_Code.py
@@ -1,9 +1,6 @@
 from __future__ import division
 import csv
-import matplotlib as mpl
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cbook as cbook
 from sys import argv
 from copy import copy, deepcopy
 from numpy.linalg import inv
