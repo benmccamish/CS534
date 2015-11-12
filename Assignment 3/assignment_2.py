baee88e379e5c4cc0b72b0c16d538674d58c7929
lassettc <lassettc@onid.oregonstate.edu>
2015-11-11 18:51:53 -0800

    Finished Bagging portion
    There is an error that occurs if the bootstrapped samples pick the perfect combination of examples that causes information gain to go to infinity.  I'm asking the TA how she wants us to handle perfect splits computationally.

--- Assignment 3/assignment_2.py
+++ Assignment 3/assignment_2.py
@@ -74,10 +74,11 @@ def correct_num(Labels):
 
 
 	if (one >= zero):
-		return one
+		return one, 'one'
 	else:
-		return zero
+		return zero, 'zero'
 
+#Added outputs zero_correct, label_zero, one_correct, label_one.  zero_correct, label_zero infers the feature is 0 and the label is the maximum correct targets in it.#
 def info_gain_acc(Data, Labels, Column):
 	total_uncertainty = uncertainty(Data, Labels)
 	
@@ -101,12 +102,14 @@ def info_gain_acc(Data, Labels, Column):
 	zero_u = uncertainty(branch_zero, branch_zero_labels)
 	one_u = uncertainty(branch_one, branch_one_labels)
 
-	zero_correct = correct_num(branch_zero_labels)
-	one_correct = correct_num(branch_one_labels)
+	zero_correct, label_zero = correct_num(branch_zero_labels)
+	one_correct, label_one = correct_num(branch_one_labels)
 	acc = float(zero_correct + one_correct) / float(len(Data))
 
 	gain = total_uncertainty - (zero_portion * zero_u) - (one_portion * one_u)
-	return gain, acc
+	#print zero_correct, label_zero, one_correct, label_one
+	
+	return gain, acc, zero_correct, label_zero, one_correct, label_one
 
 
 def Bootstrap_Sampling(Train_X, Train_Y, size):
@@ -134,41 +137,108 @@ def Bootstrap_Sampling(Train_X, Train_Y, size):
 
 	
 	
-def Correct_Stump(Train_DataX, Train_DataY):	
+def Correct_Stump(Train_DataX, Train_DataY, Test_X, Test_Y):	
 	Gains = []
-
+	Target_Guess = []
 	for c in range(len(Train_DataX[0])):
 
-		info, train_acc = info_gain_acc(Train_DataX, Train_DataY, c)
+		info, train_acc, zero_correct, label_zero, one_correct, label_one = info_gain_acc(Train_DataX, Train_DataY, c)
 		Gains.append(info)
 	max_value = max(Gains)
 	max_index = Gains.index(max_value)
-	print max_index
 
 
-
-
-
+	Gain, Accuracy, zero_correct, label_zero, one_correct, label_one = info_gain_acc(Test_X, Test_Y, max_index)
+	
+	for x in range(0, len(Test_Y)):
+		if Test_X[x][max_index] == 1:
+			if label_one == 'zero':
+				Target_Guess.append(0)
+			else:
+				Target_Guess.append(1)
+		else:
+			if label_zero == 'one':
+				Target_Guess.append(1)
+			else:
+				Target_Guess.append(0)
+	
+	return Target_Guess
+	
 
 
 def problem_1(Train_X, Train_Y, Test_X, Test_Y):
+
 	for c in range(len(Train_X[0])):
-		info, train_acc = info_gain_acc(Train_X, Train_Y, c)
-		who_cares, test_acc = info_gain_acc(Test_X, Test_Y, c)
-		print("%f \t%f" % (info, test_acc))
+		info, train_acc, zero_correct, label_zero, one_correct, label_one = info_gain_acc(Train_X, Train_Y, c)
+		who_cares, test_acc,  zero_correct, label_zero, one_correct, label_one = info_gain_acc(Test_X, Test_Y, c)
+		print("%f \t%f" % (who_cares, test_acc))
+
 
 def problem_2Bagging(Train_X, Train_Y, Test_X, Test_Y, size):	
 	Boot_StrapsX, Boot_StrapsY = Bootstrap_Sampling(Train_X, Train_Y, size)	
-	
-
+	Bootstrapped_TargetListTEST = []
+	Bootstrapped_TargetListTRAIN = []
+	Predicted_TargetsTEST = []
+	Predicted_TargetsTRAIN = []
 	for x in range(0, size):
 
-		Correct_Stump(Boot_StrapsX[x], Boot_StrapsY[x])
+		Current_Targets = Correct_Stump(Boot_StrapsX[x], Boot_StrapsY[x], Test_X, Test_Y)
+		Bootstrapped_TargetListTEST.append(Current_Targets)
+	
+	for y in range(0, size):
+
+		Current_Targets = Correct_Stump(Boot_StrapsX[y], Boot_StrapsY[y], Train_X, Train_Y)
+		Bootstrapped_TargetListTRAIN.append(Current_Targets)
+	
+
+	for z in range(0, len(Test_Y)):
+		Testcount_1 = 0
+		Testcount_0 = 0
+		for s in range(0, size):
+			if Bootstrapped_TargetListTEST[s][z] == 1:
+				Testcount_1 += 1
+			else:
+				Testcount_0 += 1
+				
+		if Testcount_1 >= Testcount_0:
+			Predicted_TargetsTEST.append(1)
+		else:
+			Predicted_TargetsTEST.append(0)
+			
+
+	
+	for a in range(0, len(Train_Y)):
+		Traincount_1 = 0
+		Traincount_0 = 0
+		for b in range(0, size):
+			if Bootstrapped_TargetListTRAIN[b][a] == 1:
+				Traincount_1 += 1
+			else:
+				Traincount_0 += 1
+				
+		if Traincount_1 >= Traincount_0:
+			Predicted_TargetsTRAIN.append(1)
+		else:
+			Predicted_TargetsTRAIN.append(0)
+			
+	count_Test = 0
+	for e in range(0, len(Test_Y)):
+		if Test_Y[e] == Predicted_TargetsTEST[e]:
+			count_Test += 1
+	print count_Test, len(Test_Y)
 		
-		
-		
+	count_Train = 0
+	for r in range(0, len(Train_Y)):
+		if Train_Y[r] == Predicted_TargetsTRAIN[r]:
+			count_Train += 1
+	print count_Train, len(Train_Y)		
+	
 def stump_accuracy(Data, Labels, Column):
 	pass
+	
+	
+	
+	
 
 def main():
 	size = 5
