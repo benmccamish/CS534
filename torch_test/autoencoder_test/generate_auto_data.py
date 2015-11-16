64caa5c0260966fcc4a877d519810bd5726b1409
Trevor Fiez <fieztr@gpu-bart.eecs.oregonstate.edu>
2015-11-16 09:00:15 -0800

    Sort of finished function to generate training data

--- torch_test/autoencoder_test/generate_auto_data.py
+++ torch_test/autoencoder_test/generate_auto_data.py
@@ -10,8 +10,18 @@ import cv2
 
 
 
-def write_frames(video_data, data_dir, data_file):
-	
+def write_frames(video_data, output_dir):
+	for i in range(len(video_data)):
+		my_vid = VideoCapture(video_data[0])
+		my_vid.set(cv2.cv.CAP_PROP_POS_FRAMES, video_data[1])
+		
+		for j in range(len(120)):
+			stuff, im = my_vid.read()
+			output_name = output_dir + str(i) + "_" + str(j) + ".png"
+			cv2.imwrite(output_name, im)
+		
+		my_vid.release()
+		
 	
 
 
@@ -29,7 +39,7 @@ def main(argc, argv):
 	video_data = []
 	read_data(argv[1], video_data)
 
-	write_frames(video_data, argv[2], argv[3])
+	write_frames(video_data, argv[2])
 
 if __name__ == "__main__":
 	main(len(sys.argv), sys.argv)
